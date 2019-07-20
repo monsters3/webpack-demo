c37c79d18ff3d70eb13ea27b6b01fe8af907f65b
darkbright <foxtori8946@gmail.com>
2019-07-20 14:38:00 +0900

    second commit

---
+++ src/js/circle.js
@@ -0,0 +1,5 @@
+// commonJS 개별모듈화 => named export
+
+export var area = (r) => Math.PI * r *r;
+
+export var circumference = (r) => 2* Math.PI * r;
---
+++ src/js/cubic.js
@@ -0,0 +1,5 @@
+// commonJs 전체 모듈화 => es6 default export
+
+var cubic = (x) => x * x * x;
+
+export default cubic;
